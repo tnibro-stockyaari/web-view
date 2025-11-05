c6a04b954682a41600a302dd35ec1839c76a3211
tnibro-stockyaari <reactjs.stockyaari@gmail.com>
2025-11-05 15:17:23 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center text-primary">Bootstrap Working ✅</h1>
+      <button className="btn btn-success mt-3">Test Button</button>
+    </div>
+  );
+}
+
+export default Home;
